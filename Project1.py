13b79005f5744b9d7a21dea5add2cfcd50a40e6f
KuzminaKate <katrina_rina@mail.ru>
2021-11-27 00:14:15 +0300

    v1

---
+++ Project1.py
@@ -0,0 +1,10 @@
+import random
+a=1
+b=100
+c=[]
+for i in range(b):
+    r=random.randint(a,b)
+    c.append(r)
+print("Получившийся массив: ", c)
+d=max(c)
+print("Максимальное число в масиве: ", d)
